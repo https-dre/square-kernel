e56b7d855eff637b497307f15f829a5fc76308d3
https-dre <diaso.andre@outlook.com>
2024-06-08 22:36:34 -0300

    add new_color_code to main method

--- src/main.c
+++ src/main.c
@@ -6,9 +6,9 @@ void kernel_main() {
     process_init();
     scheduler_init();
     print("Welcome to square-kernel!\n");
-    set_vga_color_code(10);
+    set_vga_color_code(new_color_code(Cyan, Black));
     print("We are in protected mode!\n");
-
+    
     while( 1 );
 }
 
