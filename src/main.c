4ad9fa243869e6aa7e968952be4e9e1e6fbdbabd
https-dre <diaso.andre@outlook.com>
2024-06-10 19:29:28 -0300

    add println and colors.h to main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include "vga_buffer.h"
 #include "scheduler.h"
+#include "colors.h"
 
 void kernel_main() {
     vga_init();
     process_init();
     scheduler_init();
-    print("Welcome to square-kernel!\n");
+    println("Welcome to square-kernel!");
     set_vga_color_code(new_color_code(Cyan, Black));
     print("We are in protected mode!\n");
     
