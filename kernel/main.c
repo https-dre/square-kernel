0bef209995bbd1c832dea5f461a016dfccb2ff67
https-dre <diaso.andre@outlook.com>
2024-06-11 21:26:11 -0300

    add basic paging implementation to start point of kernel
    - the processA and processB change eax forever
    - heap_init and paging_init should be initilized with sucess to
      starts the process manager

--- kernel/main.c
+++ kernel/main.c
@@ -4,17 +4,38 @@
 #include "heap.h"
 #include "paging.h"
 
+void processA();
+void processB();
+
 void kernel_main() {
-    /* heap_init();
-    paging_init(); */
+    heap_init();
+    paging_init();  
     vga_init();
-    /* process_init();
-    scheduler_init(); */
-    println("Welcome to square-kernel!");
-    set_vga_color_code(new_color_code(Cyan, Black));
-    print("We are in protected mode!\n");
+    process_init();
+    scheduler_init();
+    set_vga_color_code(new_color_code(Black, LightGreen));
+    println("running square-kernel");
+    setDefaultColor();
+    process_create(&processA);
+    process_create(&processB);
     
     while( 1 );
+}
+
+void processA()
+{
+    println("Running Process A, adicionando 11 ao eax");
+
+    while ( 1 )
+        asm( "mov $11, %eax" );
+}
+
+void processB()
+{
+    println("Running Process B, adicionando 22 ao eax");
+
+    while ( 1 )
+        asm( "mov $22, %eax" );
 }
 
 void interrupt_handler(int interrupt_number) {
