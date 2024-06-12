2d3af7d322b374d7080b664c5cdc152f0b8ef45f
https-dre <diaso.andre@outlook.com>
2024-06-11 21:29:27 -0300

    add a basic print with assembly using bios interruption

---
+++ arch/x86/print.asm
@@ -0,0 +1,24 @@
+start:
+  mov ax, cs
+  mov ds, ax
+    
+  mov si, hello_string
+  call print_string
+    
+  jmp $
+
+print_string:
+  mov ah, 0Eh
+
+print_char:
+  lodsb
+    
+  cmp al, 0
+  je done
+    
+  int 10h
+    
+  jmp print_char
+
+done:
+  ret
