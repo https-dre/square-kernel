942aa2fc5b53ed4ca7707e3557140defb2c09394
= <=>
2024-06-14 18:22:54 -0400

    add comments in Portuguese

--- arch/x86/init/starter.asm
+++ arch/x86/init/starter.asm
@@ -55,9 +55,20 @@ init_video_mode:
     int 10h
 
     ret
+
+; PIC (Programmable Interrupt Controller)
+; O Controlador de Interrupção Programável é responsável por
+; receber sinais de interrupção de diversos dispositivos de
+; hardware (teclado, mouse, placas de rede) e encaminhá-los ao
+; processador para serem tratados pela ISR correspondente.
+
+; Cada linha de interrupção na PIC está associada a um número de IRQ específico.
+; Quando um dispositivo de hardware precisa de atenção, ele envia um sinal para sua
+; linha de IRQ correspondente.
+
 remap_pic:
     mov al, 0x11        ; Início da inicialização do PIC mestre
-    out 0x20, al
+    out 0x20, 
     mov al, 0x11        ; Início da inicialização do PIC escravo
     out 0xA0, al
 
@@ -77,8 +88,14 @@ remap_pic:
     out 0xA1, al
 
     ; Mascarar todas as interrupções exceto a do timer (IRQ0)
-    mov al, 0xFE
-    out 0x21, al
+    mov al, 0xFE ; 0xFE = 1111 1110 em binário, mascara 
+    ; todas as IRQS exceto a do timer
+    out 0x21, al ; Aplica a máscara de interrupções no PIC mestre
+
+    ; Máscara para permitir a IRQ do teclado (IRQ1)
+    ; in al, 0x21 ; lê o registrador de máscara atual do PIC mestre
+    ; and al, 0xFD ; limpa o bit 1 para habilitar a IR1 (teclado)
+    ; out 0x21, al ; aplica a nova máscara
 
     ; Mascarar todas as interrupções do PIC escravo
     mov al, 0xFF
