4a08436fd133ec58e110500617ca376d0ad7a30e
https-dre <diaso.andre@outlook.com>
2025-02-13 18:19:14 -0300

    fix heap initialization and memory allocation functions
    - Corrected heap initialization to set correct size.
    - Added return statement in kfree function.
    - Added comments for better readability.

--- include/heap.h
+++ include/heap.h
@@ -1,3 +1,13 @@
-extern unsigned int heap_base;
+#define HEAP_START  0x100000
+#define HEAP_LIMIT  0x200000  // limite para a heap
+#define NULL ((void*)0)
+
+typedef struct Block {
+    int size;         // Tamanho do bloco (excluindo a estrutura)
+    struct Block* next;  // Próximo bloco na lista
+    int free;            // 1 = Livre, 0 = Ocupado
+} Block;
+
+extern Block* heap_base;
 void heap_init();
-int kalloc(int);
+void* kalloc(int);
--- mm/heap/heap.c
+++ mm/heap/heap.c
@@ -1,15 +1,49 @@
 #include "heap.h"
+#include "vga_buffer.h"
 
-unsigned int heap_base;
+Block* heap_base = (Block*)HEAP_START;
+#define KB 1024
 
-void heap_init()
-{
-    heap_base = 0x100000;
+void heap_init() {
+    heap_base->size = 8;
+    heap_base->next = NULL;
+    heap_base->free = 1;
+    println("Heap initialized");
 }
 
-int kalloc(int bytes) {
-    unsigned int new_object_address = heap_base;
+void* kalloc(int bytes) {
+    Block* current = heap_base;
 
-    heap_base += bytes;
-    return new_object_address;
+    while (current) {
+        if (current->free == 1 && current->size >= bytes) {
+            // Dividir o bloco se sobrar espaço suficiente para um novo bloco
+            if (current->size > bytes + sizeof(Block)) {
+                Block* new_block = (Block*)((unsigned int)current + sizeof(Block) + bytes);
+                new_block->size = current->size - bytes - sizeof(Block);
+                new_block->free = 1;
+                new_block->next = current->next;
+                current->next = new_block;
+            }
+
+            current->size = bytes;
+            current->free = 0;
+            println("Mais memória alocada!");
+            return (void*)((unsigned int)current + sizeof(Block));
+        }
+        current = current->next;
+    }
+
+    return (void*)-1;
+}
+
+int kfree(void* ptr) {
+    if (!ptr) return -1;
+
+    Block* block = (Block*)((unsigned int)ptr - sizeof(Block));
+    block->free = 1;
+
+    if (block->next && block->next->free) {
+        block->size += block->next->size + sizeof(Block);
+        block->next = block->next->next;
+    }
 }
