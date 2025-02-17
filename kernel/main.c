f3585cd30cb1ff68d5180204b4569a307764cdc0
https-dre <diaso.andre@outlook.com>
2025-02-17 19:33:38 -0300

    remove "heap.h" symbols

--- kernel/main.c
+++ kernel/main.c
@@ -1,11 +1,9 @@
 #include "vga_buffer.h"
 #include "colors.h"
-#include "heap.h"
 #include "paging.h"
 
 void kernel_main() {
     vga_init();
-    //heap_init();
     paging_init();
     set_vga_color_code(color_code(Black, LightGreen));
     println("Kernel Ready!");
