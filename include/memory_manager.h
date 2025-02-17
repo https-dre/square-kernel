2d0604c1859d11786f162ce59981fb9cb4bfae4c
https-dre <diaso.andre@outlook.com>
2025-02-17 19:29:52 -0300

    adds the memory manager header and removes duplicate page definitions

---
+++ include/memory_manager.h
@@ -0,0 +1,25 @@
+#define PAGE_SIZE 4096
+#define TOTAL_PAGES 1024
+
+#define PDE_NUM 3
+#define PTE_NUM 1024
+
+#include "stdint.h"
+#include "stddef.h"
+
+extern uintptr_t  *page_directory;
+
+#define PAGE_SIZE 4096
+#define TOTAL_PAGES 1024
+
+#define BASE_VIRTUAL 0x100000
+#define BASE_FISICA  0x00000000  // Endereço real onde começa a memória física
+
+#define HEAP_MAGIC 0x12345678
+#define HEAP_MIN_SIZE 0x70000
+#define HEAP_INDEX_SIZE 0x20000
+
+typedef struct block {
+    size_t size;       // Tamanho do bloco
+    struct block* next; // Ponteiro para o próximo bloco
+} block_t;
--- include/page_allocator.h
+++ include/page_allocator.h
@@ -1,14 +1,9 @@
 #ifndef MEMORY_ALLOCATOR_H
 #define MEMORY_ALLOCATOR_H
 
-#include <stddef.h>
-
-#define PAGE_SIZE 4096
-#define TOTAL_PAGES 1024
-
 void init_page_allocator();
 void* alloc_page();
 void free_page(void* page);
 int debug_pages();
 
-#endif // MEMORY_ALLOCATOR_H
+#endif
