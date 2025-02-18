03a8db437360af80b5cbac53cc0fe833906cda43
https-dre <diaso.andre@outlook.com>
2025-02-18 17:35:00 -0300

    adds a basic implementation for heap
    - with only basic memory allocation functions

--- include/liballoc.h
+++ include/liballoc.h
@@ -1,28 +1,18 @@
-#ifndef HEAP_H
-#define HEAP_H
+#include <stddef.h>
+#include <stdint.h>
 
-#include "stddef.h"
-#include "stdint.h"
+typedef struct heap_block {
+    size_t size;
+    struct heap_block *next;
+    int free;
+} heap_block_t;
 
-typedef struct {
-    unsigned int magic;    // Magic number for error checking and identification.
-    unsigned char is_hole; // 1 if this is a hole. 0 if this is a block.
-    size_t size;           // Size of the block, including the end footer.
-} header_t;
+extern uintptr_t heap_top;
 
-typedef struct {
-    unsigned int magic;    // Magic number, same as in header_t.
-    header_t *header;      // Pointer to the block header.
-} footer_t;
+extern void heap_init(uintptr_t start);
+extern uintptr_t find_free_block(size_t size);
+extern uintptr_t push_block(size_t size);
 
-typedef struct {
-    void *start_address;   // The start of our allocated space.
-    size_t size;           // The size of our allocated space.
-} heap_t;
-
-heap_t *create_heap(uintptr_t start, uintptr_t end, uintptr_t max);
-void *malloc(size_t size, heap_t *heap);
-void free(void *p, heap_t *heap);
-void *realloc(void *p, size_t size, heap_t *heap);
-
-#endif
+extern uintptr_t kmalloc(size_t size);
+extern int kfree(uintptr_t addr);
+//extern uintptr_t krealloc(uintptr_t addr, size_t size);
