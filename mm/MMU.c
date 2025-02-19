1cda60437662fe581f9fa7d2b83e5ac7c9624298
https-dre <diaso.andre@outlook.com>
2025-02-19 19:14:46 -0300

    corrects MMU initialization to correctly check heap home page allocation

--- mm/MMU.c
+++ mm/MMU.c
@@ -1,21 +1,14 @@
 #include <memory/memory_manager.h>
-#include <memory/paging.h>
 #include <memory/liballoc.h>
 #include <memory/page_allocator.h>
 #include <video/vga_buffer.h>
 
 int init_mmu() {
-    paging_init();
-    uintptr_t heap_start = alloc_page();
-    if(heap_start == 0) {
-        errorPrint("Failed to allocate heap start page");
-    } else {
-        heap_init(heap_start);
+    uintptr_t heap_start = (uintptr_t)alloc_page();
+    if(heap_start == NULL) {
+        errorPrint("HEAP_START == NULL");
+        println("MMU error!");
+        return -1;
     }
-
-    #ifdef DEBUG
-    uint16_t *number = (uint16_t*)kmalloc(sizeof(uint16_t));
-    *number = 18;
-    kfree((uintptr_t)number);
-    #endif
+    heap_init(heap_start);
 }
